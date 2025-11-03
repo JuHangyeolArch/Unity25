a7272b7357efaf34a5b9a455381fb63f24394001
JuHangyeolArch <168800809+JuHangyeolArch@users.noreply.github.com>
2025-11-03 17:49:29 +0900

    ch06_1103

--- CH6/Assets/2.Scripts/CameraController.cs
+++ CH6/Assets/2.Scripts/CameraController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform playerTransform;
+   //private GameObject player;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+     //   this.player = GameObject.Find("cat");
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Vector2 playerPos = this.playerTransform.position;
         
+        transform.position = new Vector3 (transform.position.x,playerPos.y + 4f,transform.position.z);
     }
 }
--- CH6/Assets/2.Scripts/PlayerController.cs
+++ CH6/Assets/2.Scripts/PlayerController.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using System.Runtime.CompilerServices;
+using UnityEditor.Animations;
 
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public Rigidbody2D rb;
-    public float jumpForce = 300f;
+    public float jumpForce = 400f;
     public float walkForce = 30f;
 
     public float maxWalkSpeed = 2f;
 
     private int key = 0;
     private float speedX = 0f;
+    private float speedY = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +30,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) && 
+            this.rb.velocity.y==0) 
         {
 
             this.rb.AddForce(transform.up * jumpForce);
+            this.animator.SetTrigger("Jump Trigger");
         }
 
 
@@ -44,8 +50,9 @@ public class PlayerController : MonoBehaviour
             this.key = 1;
         }
 
-        this.speedX = Mathf.Abs(rb.velocity.x);
-        if(speedX < maxWalkSpeed)
+        this.speedX = Mathf.Abs(this.rb.velocity.x);
+        this.speedY = Mathf.Abs(this.rb.velocity.y);
+        if (speedX < maxWalkSpeed)
         {
             this.rb.AddForce(transform.right * this.walkForce * this.key);
         }
@@ -57,7 +64,26 @@ public class PlayerController : MonoBehaviour
         
         }
 
-        this.animator.speed = this.speedX/2;
+        if (this.rb.velocity.y == 0)
+        {
+            this.animator.speed = this.speedX / 2;
+        }
+        else {
+            this.animator.speed = this.speedY / 2;
+        }
+
+        if (transform.position.y < -10f) 
+        {
+            string sceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(sceneName);
+        }
 
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Goal!");
+        SceneManager.LoadScene("Clear Scene");
+    }
+
 }
